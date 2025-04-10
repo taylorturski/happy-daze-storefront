f8725e45266fd373bd406cb46a643b22a5a93723
taylorturski <tturski47@gmail.com>
2025-04-10 09:22:15 -0500

    boiler plate pages added

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Learn about the story and mission behind Happy Daze Golf.</p>
+    </div>
+  );
+}
---
+++ app/custom-shop/page.tsx
@@ -0,0 +1,8 @@
+export default function CustomShopPage() {
+  return (
+    <div>
+      <h1>Custom Shop</h1>
+      <p>This is where customers will customize and order putters.</p>
+    </div>
+  );
+}

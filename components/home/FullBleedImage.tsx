1da944987522d8d017a335792a454d86b318919e
taylorturski <tturski47@gmail.com>
2025-04-25 13:15:19 -0500

    refactor homepage image for LCP

--- components/home/FullBleedImage.tsx
+++ components/home/FullBleedImage.tsx
@@ -14,20 +14,20 @@ export default function FullBleedImage({
   imageSrc,
   alt = "",
   children,
-  priority = false,
 }: FullBleedImageProps) {
   return (
     <section className="relative w-full h-[79svh] overflow-hidden">
-      <Image
-        src={imageSrc}
-        alt={alt}
-        fill
-        priority={priority}
-        className="object-cover object-center brightness-[0.9] contrast-[1.1] saturate-[0.85] z-0"
-      />
-
+      <div className="absolute inset-0 z-0">
+        <Image
+          src={imageSrc}
+          alt={alt}
+          fill
+          priority
+          sizes="100vw"
+          className="object-cover object-center brightness-[0.9] contrast-[1.1] saturate-[0.85]"
+        />
+      </div>
       <div className="absolute inset-0 bg-black/30 z-10" />
-
       <div className="relative z-20 flex flex-col items-center justify-center h-full text-white text-center px-4 tracking-tight font-bold uppercase text-2xl">
         {children}
       </div>
